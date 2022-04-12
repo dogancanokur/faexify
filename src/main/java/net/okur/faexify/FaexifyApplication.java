3d9b1aca80b6906ddba44032130b5dffc654b5ed
dogancan.okur <dogancan.okur@gmail.com>
2022-04-13 02:52:06 +0300

    init project

---
+++ src/main/java/net/okur/faexify/FaexifyApplication.java
@@ -0,0 +1,13 @@
+package net.okur.faexify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaexifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaexifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/okur/faexify/FaexifyApplicationTests.java
@@ -0,0 +1,13 @@
+package net.okur.faexify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaexifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
